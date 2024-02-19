862b9d7e768d15e64eeac1fe6d2e7c5f6bfb15d5
João Vitor Nunes da Silva <135175090+Joao-Vitor-Nunes@users.noreply.github.com>
2024-02-19 11:42:00 -0400

    att

--- interact.js
+++ interact.js
@@ -7,8 +7,10 @@ btn.addEventListener('click', () => {
     let tarefa = document.getElementById('text')
     let msg = 
     `
-    <div class= "flex flex-colunm pl-5 gap-3">
-    <p class= "text-amber-500 pt-2 pl-3 text-xl font-semibold" >${tarefa.value}</p>
+    <div class= "flex pl-5 gap-3">
+        <p class= "flex-1 text-amber-500 pt-2 pl-3 text-xl font-semibold" >${tarefa.value}</p>
+        <button type="button" class= "my-auto w-7 h-6 px-2 rounded bg-amber-600 hover:bg-amber-700 transition delay-150 font-semibold">✓</button>
+        <button type="button" class= "my-auto w-7 h-6 px-2 rounded bg-amber-600 hover:bg-amber-700 transition delay-150 font-semibold">X</button>
     </div>
 
     `
